24293f2ba8d8d0fd05bda9f3ac6a58e59c02e5ab
yukatayu <yukatayu.dev@gmail.com>
2019-08-21 15:09:52 +0900

    refactor: use parser in main

--- main.cpp
+++ main.cpp
@@ -1,33 +1,13 @@
 #include <iostream>
 #include "Rustic_SAT_Solver/data.hpp"
 #include "Rustic_SAT_Solver/solver.hpp"
+#include "parser.hpp"
 
 int main(){
-	// prepare
-	/**
-	 * A: !B && !C
-	 * B: !A
-	 * C: !B
-	 */
-	/**
-	 * (!A || !B) && (!A || !C) && (B || C || A)
-	 * (!A || !B) && (B || A)
-	 * (!B || !C) && (C || B)
-	 */
-	auto clauses = ClauseSet({
-		// A
-		{{ "-a"_L, "-b"_L }},
-		{{ "-a"_L, "-c"_L }},
-		{{  "b"_L,  "c"_L,  "a"_L }},
-		// B
-		{{ "-a"_L, "-b"_L }},
-		{{  "b"_L,  "a"_L }},
-		// C
-		{{ "-b"_L, "-c"_L }},
-		{{  "c"_L,  "b"_L }}
-	});
+	// std::set<Proposition>, ClauseSet
+	auto [prop, CNF] = parse("!((a&(b))|!(c|d&e&!f))");
 	// solve
-	auto [isSAT, conds] = solve(clauses);
+	auto [isSAT, conds] = solve(CNF);
 	// print
 	std::cout << (isSAT ? "This is SAT" : "This is not SAT") << std::endl;
 	for(auto&& cond : conds){
